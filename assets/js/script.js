07af2c1bbe1827d62b26192e34d092e451d16caf
JosephW143 <josephwright1436@gmail.com>
2022-06-30 23:02:20 -0400

    add pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
+/*
+create functions to get an answer for if user wants wants lowercase, uppercase, numbers, and/or special characters in their passowrd as well as the length
+- promts should ask if user wants to add characters through a window prompt that will be confimed or cancled 
+- function should return a value that will be called in the generate passowrd function 
+
+create a random number function that calls on the values to know what password to generate random characters for each value 
+
+create a generate password funciton that calls on other functions to generate a password 
+*/
 // Assignment code here
-const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowerCase = 'abcdefghijklmnopqrstuvqxyz';
-const numValue = '1234567890';
-const specialChar = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lower = 'abcdefghijklmnopqrstuvqxyz';
+const num = '1234567890';
+const special = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+
+var  upperCase = function() {
+  var upperCase = window.prompt("how many characters would you like. must be between 8 and 128")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
